c0307efc19e1a216405b384ba3c604389c871fc9
Matthew Demchuk <mdemchuk478@gmail.com>
2022-11-06 23:52:37 -0500

    Added results to Game of 27 Though need to find a way to create these games with no global variables

--- src/js/dice27.js
+++ src/js/dice27.js
@@ -1,21 +1,26 @@
 const baseTotal = 27;
 var currTotal = 27;
-var turn = 0;
+var turn = true;
+var rolls = [];
 var scoreboard = [0,0];
 
 document.getElementById("overalScore").innerHTML = currTotal;
 document.getElementById("player0").innerHTML = scoreboard[0];
 document.getElementById("player1").innerHTML = scoreboard[1];
+//set array to size
+for(let i = 0; i <= baseTotal; i++){
+    rolls[i] = 0;
+}
 
 
 function roll(){
-
     //get random number between 1-6
     let rollValue = Math.floor(Math.random() * 6) + 1;
     let player;
 
     document.getElementById("dice").src = "../images/dice" + rollValue + ".png";
 
+    //determine whos turn it is and prep for next roll
     if(turn){
         player = 1;
         turn = false;
@@ -24,6 +29,9 @@ function roll(){
         player = 0;
         turn = true;
     }
+
+    rolls[0]++;
+    rolls[currTotal]++;
     
     scoreboard[player] = scoreboard[player] + calculateScore(rollValue);
     document.getElementById("player" + player).innerHTML = scoreboard[player];
@@ -49,6 +57,12 @@ function checkScore(){
             document.getElementById("overalScore").innerHTML = "Player 2 Wins!";
         }
         document.getElementById("resetButton").hidden = false;
+
+        for(let i = 0; i <= baseTotal; i++){
+            document.getElementById("roll" + i).innerHTML = rolls[i];
+        }
+        document.getElementById("results").hidden = false;
+        
     }
 }
 
@@ -56,9 +70,13 @@ function reset(){
     currTotal = baseTotal;
     scoreboard[0] = 0;
     scoreboard[1] = 0;
+    turn = true;
     document.getElementById("resetButton").hidden = true;
     document.getElementById("rollButton").hidden = false;
     document.getElementById("overalScore").innerHTML = currTotal;
     document.getElementById("player0").innerHTML = scoreboard[0];
     document.getElementById("player1").innerHTML = scoreboard[1];
+    document.getElementById("results").hidden = true;
 }
+
+
