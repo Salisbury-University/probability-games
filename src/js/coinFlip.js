c7d710da32885752553b5a983ee1c84140d906dd
Sean Kouyeas <skouyeas@gmail.com>
2022-11-15 11:13:31 -0500

    Added multiple sets of flips to total count

--- src/js/coinFlip.js
+++ src/js/coinFlip.js
@@ -1,50 +1,97 @@
+		// determines if coin will be weighted or not
+		var isWeighted = Math.floor(Math.random() * 2) + 1;
+
+		// counters for both sides
+		var headCount = 0;
+		var tailCount = 0;
+
+		// determines amount of weight for the heavier side 
+		// (anywhere from 70-30 to 57-43, with the higher number being the heavier side)
+		var min = 57;
+		var max = 71;
+		var difference = max - min;
+		var weightHeavierSide = Math.floor(Math.random() * difference) + min;
+
+		// determines if tails or heads is the heavier side (1 for heads and 2 for tails)
+		var heavierSide = Math.floor(Math.random() * 2) + 1;
+		
 		// Function to flip coin
 		function flipCoin() {
-			
-			let randomNumber = Math.random();
-			let weighted = Math.floor(Math.random() * 2) + 1;
+			// reset answer
+			document.querySelector("h2").innerHTML = "";
 
-			// coin is heads
-			if (randomNumber < .5) {
-				document.querySelector("h1").innerHTML = "You got HEADS!";
-				document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
-			} 
-			// coin is tails
+			if(isWeighted == 1) {
+				// heavier side is heads
+				if(heavierSide == 1) {
+       				let num = Math.floor(Math.random() * 100) + 1;
+					
+					// if weighted coin is heads
+        			if (num <= weightHeavierSide) {
+          				document.querySelector("h1").innerHTML = "You got HEADS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+						headCount += 1;
+        			} 
+					// if weighted coin is tails
+					else {
+						document.querySelector("h1").innerHTML = "You got TAILS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+						tailCount += 1;
+        			}
+      				
+      				document.getElementById("headTotal").innerHTML = headCount;
+					document.getElementById("tailTotal").innerHTML = tailCount;
+				}
+
+				// heavier side is tails
+				else {
+					let num = Math.floor(Math.random() * 100) + 1;
+					// weighted coin is tails
+					if (num <= weightHeavierSide) {
+						document.querySelector("h1").innerHTML = "You got TAILS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+						tailCount += 1;
+					} 
+					// weighted coin is heads
+					else {
+						document.querySelector("h1").innerHTML = "You got HEADS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+						headCount += 1;
+					}
+					document.getElementById("headTotal").innerHTML = headCount;
+					document.getElementById("tailTotal").innerHTML = tailCount;
+				}
+			}
+
 			else {
-				document.querySelector("h1").innerHTML = "You got TAILS!";
-				document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+				let num = Math.floor(Math.random() * 100) + 1;
+					
+				// if regular coin is heads
+        		if (num <= 50) {
+					document.querySelector("h1").innerHTML = "You got HEADS!";
+					document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+          			headCount += 1;
+        		} 
+				// if regular coin is tails
+				else {
+					document.querySelector("h1").innerHTML = "You got TAILS!";
+					document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+          			tailCount += 1;
+				}
+      			document.getElementById("headTotal").innerHTML = headCount;
+				document.getElementById("tailTotal").innerHTML = tailCount;
 			}
 		}
-
-		var isWeighted = 0;
 
 		// Function to flip coin multiple times
 		function flipCoinMultiple() {
 			// reset answer
 			document.querySelector("h2").innerHTML = "";
 
-			// determines if coin is weighted or not
-			isWeighted = Math.floor(Math.random() * 2) + 1;
-
 			// gets amount of flips from user
 			let qty = document.getElementById("quantity").value;
 
 			// if coin is weighted
-			if (isWeighted == 1) {
-				// counters for both sides
-				let headCount = 0;
-				let tailCount = 0;
-
-				// determines amount of weight for the heavier side 
-				// (anywhere from 70-30 to 57-43, with the higher number being the heavier side)
-				let min = 57;
-				let max = 71;
-				let difference = max - min;
-				let weightHeavierSide = Math.floor(Math.random() * difference) + min;
-
-				// determines if tails or heads is the heavier side (1 for heads and 2 for tails)
-				let heavierSide = Math.floor(Math.random() * 2) + 1;
-								
+			if (isWeighted == 1) {								
       			if(heavierSide == 1) {
 					for (let i = 0; i < qty; i++) {
        					let num = Math.floor(Math.random() * 100) + 1;
@@ -82,9 +129,6 @@
 
 			// if coin is not weighted
 			else {
-				let headCount = 0;
-				let tailCount = 0;
-
       			for (let i = 0; i < qty; i++) {
        				let num = Math.floor(Math.random() * 100) + 1;
 					
@@ -107,6 +151,14 @@
 				document.querySelector("h2").innerHTML = "You are Correct!";
 			else
 				document.querySelector("h2").innerHTML = "You are Incorrect";
+
+			document.getElementById("headTotal").innerHTML = 0;
+			document.getElementById("tailTotal").innerHTML = 0;
+			headCount = 0;
+			tailCount = 0;
+			isWeighted = Math.floor(Math.random() * 2) + 1;
+			weightHeavierSide = Math.floor(Math.random() * difference) + min;
+			heavierSide = Math.floor(Math.random() * 2) + 1;
 		}
 
 		function ifNotWeighted() {
@@ -114,4 +166,12 @@
 				document.querySelector("h2").innerHTML = "You are Incorrect";
 			else
 				document.querySelector("h2").innerHTML = "You are Correct!";
+
+			document.getElementById("headTotal").innerHTML = 0;
+			document.getElementById("tailTotal").innerHTML = 0;
+			headCount = 0;
+			tailCount = 0;
+			isWeighted = Math.floor(Math.random() * 2) + 1;
+			weightHeavierSide = Math.floor(Math.random() * difference) + min;
+			heavierSide = Math.floor(Math.random() * 2) + 1;
 		}
