297fa646409c3868967daefa155c4fdfb46202b6
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2023-03-01 12:18:49 -0500

    Dice27 Updates Begun simulation aspect of the game along with fixing some bugs in the original

---
+++ src/js/dice27Sim.js
@@ -0,0 +1,12 @@
+function simulate(){
+    let userInput = document.getElementById("userInput").value;
+    if(userInput == ""){}
+    else{
+        for(let i = 0; i < userInput; i++){
+            let total = 27;
+            while(total > 0){
+
+            }
+        }
+    }
+}
