7a674b191df16c5f1224dfe1f025531164461dd7
WgocowboyS <wgocowboys@gmail.com>
2023-03-01 13:00:16 -0500

    Small changes

--- src/js/needle.js
+++ src/js/needle.js
@@ -102,8 +102,8 @@ function needleXY() {
     // .tint = color
     // yellow? 0xfcba03
     // white? 0xffffff
-    //this for loop section chooses the color of the line to be dropped
 
+    //this for loop section chooses the color of the line to be dropped
     lineInArray.lineStyle(1, 0xfcba03, 1);
     for (let k = 0; k < lines.length; k++) {
       //checks to see if the needle dropped not crosses the grid lines and changes color red
@@ -131,6 +131,7 @@ function needleXY() {
     //pushing the new line into the array
     lineArray.push(lineInArray);
   }
+
 // this is all the stats to put on the screen 
   let pi = (2.0 * needleLength) / (lineSpace * ((needleCross) / needleDrop)); // pi estimation 
   let error = Math.abs((pi - Math.PI)/Math.PI) *100; //percent error
@@ -142,8 +143,6 @@ function needleXY() {
   document.getElementById("needleDontCross").innerHTML = "Needles that Don't Cross a Line: " + (needleDrop - needleCross);
   document.getElementById("total").innerHTML = "Total Needles Dropped: " + needleDrop;
   document.getElementById("percentError").innerHTML = "Percent Error for PI: " + Math.round(error*10000)/10000 + "%";
-
-
 }
 
 //clears needles from page and removes them from the array
