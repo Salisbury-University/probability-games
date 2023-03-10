0649fa204054c9871fbbf5c09ff165d5f69f082c
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2023-03-09 19:04:41 -0500

    update

--- src/js/dice27Sim.js
+++ src/js/dice27Sim.js
@@ -44,14 +44,14 @@ let simulator = new ProbabilitySimulator();
 function simulate() {
   let userInput = document.getElementById("userInput").value;
   simulator.simulate(userInput);
-  simulator.displayStats();
+  //simulator.displayStats();
   updateChart(simulator.stats);
 
 }
 
 function reset() {
   simulator.resetStats();
-  simulator.displayStats();
+  //simulator.displayStats();
   updateChart(simulator.stats);
 
 }
