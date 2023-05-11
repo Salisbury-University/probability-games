2ac96aad02bcc8473b3a55c6c0b49268baca1fa7
QueenOfSpadz <jbarry8@gulls.salisbury.edu>
2023-05-11 08:53:39 -0400

    added space

--- src/js/needle.js
+++ src/js/needle.js
@@ -84,8 +84,8 @@ window.onload = function () {
     needleDropSound.volume = e.currentTarget.value / 100;
   });
 
-  document.getElementsByClassName("estimation")[0].innerHTML = "PI Estimation: 0";
-  document.getElementsByClassName("estimation")[1].innerHTML = "0";
+  document.getElementsByClassName("estimation")[0].innerHTML = "PI Estimation: ";
+  document.getElementsByClassName("estimation")[1].innerHTML = " ";
   document.getElementById("realPi").innerHTML = "Rounded value of PI : " + Math.round(Math.PI * 10000) / 10000;
   document.getElementsByClassName("needLength")[0].innerHTML = Math.round(needleLength * 10) / 10 + " Units"; //the  units is pixels
   document.getElementsByClassName("needLength")[1].innerHTML = Math.round(needleLength * 10) / 10; //the  units is pixels
@@ -98,7 +98,7 @@ window.onload = function () {
   document.getElementsByClassName("total")[0].innerHTML = "Total # of Needles Dropped: 0";
   document.getElementsByClassName("total")[1].innerHTML = 0;
   document.getElementsByClassName("total")[2].innerHTML = 0;
-  document.getElementById("percentError").innerHTML = "Percent Error for PI estimaton: 0";
+  document.getElementById("percentError").innerHTML = "Percent Error for PI estimaton: ";
 };
 
 
@@ -432,8 +432,8 @@ function clearNeedles() {
   neg = 0;
   pos = 0;
 
-  document.getElementsByClassName("estimation")[0].innerHTML = "PI Estimation: 0";
-  document.getElementsByClassName("estimation")[1].innerHTML = "0";
+  document.getElementsByClassName("estimation")[0].innerHTML = "PI Estimation: ";
+  document.getElementsByClassName("estimation")[1].innerHTML = " ";
   document.getElementById("realPi").innerHTML = "Rounded value of PI : " + Math.round(Math.PI * 10000) / 10000;
   document.getElementsByClassName("needLength")[0].innerHTML = Math.round(needleLength * 10) / 10 + " Units"; //the  units is pixels
   document.getElementsByClassName("needLength")[1].innerHTML = Math.round(needleLength * 10) / 10; //the  units is pixels
@@ -446,7 +446,7 @@ function clearNeedles() {
   document.getElementsByClassName("total")[0].innerHTML = "Total # of Needles Dropped: 0";
   document.getElementsByClassName("total")[1].innerHTML = 0;
   document.getElementsByClassName("total")[2].innerHTML = 0;
-  document.getElementById("percentError").innerHTML = "Percent Error for PI estimaton: 0";
+  document.getElementById("percentError").innerHTML = "Percent Error for PI estimaton: ";
 
 
   //this code removes them from the stage
