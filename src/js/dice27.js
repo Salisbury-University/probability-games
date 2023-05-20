3be6fb8833a32110083159f204bcf851784d1285
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2023-05-20 19:44:27 -0400

    working on game 27

--- src/js/dice27.js
+++ src/js/dice27.js
@@ -545,16 +545,31 @@ class ScreenManagement {
     }
     #changeColor() {
         let text = document.querySelectorAll(".text");
+        let card = document.querySelectorAll(".gameCard");
+        let menu = document.querySelectorAll(".menu");
         if (this.#color.checked) {//dark mode
-            document.body.style.backgroundColor = "#262626";
+            document.body.style.backgroundColor = "#343a40";
             for (let i = 0; i < text.length; i++) {
                 text[i].style.color = 'white';
             }
+            for (let i = 0; i < card.length; i++) {
+                card[i].style.backgroundColor = "#6C757D";
+            }
+            for (let i = 0; i < menu.length; i++) {
+                menu[i].style.backgroundColor = "#343a40";
+            }
+
             sessionStorage.setItem("theme", "dark");
         } else {//light mode
             document.body.style.backgroundColor = "#ffffff";
             for (let i = 0; i < text.length; i++) {
                 text[i].style.color = 'black';
+            }
+            for (let i = 0; i < card.length; i++) {
+                card[i].style.backgroundColor = "white"
+            }
+            for (let i = 0; i < menu.length; i++) {
+                menu[i].style.backgroundColor = "#ffffff";
             }
             sessionStorage.setItem("theme", "light");
         }
